409c6532f4fc8c3e268bb619323fc24d4f49e88a
Yasinta Romadhona <yasinta.roro@gmail.com>
2019-06-25 16:24:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -307,12 +307,9 @@
                     <!-- Single Footer Area Start -->
                     <div class="col-12 col-md-6 col-lg-3">
                         <div class="single_footer_area">
-                            <div class="footer-logo">
-                                <img src="img/core-img/logo.png" alt="">
-                            </div>
-                            <div class="copywrite_text d-flex align-items-center">
+                            <div class="footer_widget_menu">
                                 <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+Copyright &copy;<script>document.write(new Date().getFullYear());</script>
 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
                             </div>
                         </div>
@@ -322,10 +319,6 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> All r
                         <div class="single_footer_area">
                             <ul class="footer_widget_menu">
                                 <li><a href="#">About</a></li>
-                                <li><a href="#">Blog</a></li>
-                                <li><a href="#">Faq</a></li>
-                                <li><a href="#">Returns</a></li>
-                                <li><a href="#">Contact</a></li>
                             </ul>
                         </div>
                     </div>
@@ -333,27 +326,27 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> All r
                     <div class="col-12 col-sm-6 col-md-3 col-lg-2">
                         <div class="single_footer_area">
                             <ul class="footer_widget_menu">
-                                <li><a href="#">My Account</a></li>
-                                <li><a href="#">Shipping</a></li>
                                 <li><a href="#">Our Policies</a></li>
-                                <li><a href="#">Afiliates</a></li>
                             </ul>
                         </div>
                     </div>
                     <!-- Single Footer Area Start -->
-                    <div class="col-12 col-lg-5">
+                    <div class="col-12 col-sm-6 col-md-3 col-lg-2">
                         <div class="single_footer_area">
-                            <div class="footer_heading mb-30">
-                                <h6>Subscribe to our newsletter</h6>
-                            </div>
-                            <div class="subscribtion_form">
-                                <form action="#" method="post">
-                                    <input type="email" name="mail" class="mail" placeholder="Your email here">
-                                    <button type="submit" class="submit">Subscribe</button>
-                                </form>
-                            </div>
+                            <ul class="footer_widget_menu">
+                                <li><a href="#">Contact</a></li>
+                            </ul>
                         </div>
                     </div>
+                     <!-- Single Footer Area Start -->
+                     <div class="col-12 col-sm-6 col-md-3 col-lg-2">
+                        <div class="single_footer_area">
+                            <ul class="footer_widget_menu">
+                                <li><a href="#">Shipping</a></li>
+                            </ul>
+                        </div>
+                    </div>
+
                 </div>
                 <div class="line"></div>
 
@@ -362,7 +355,6 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> All r
                     <div class="row">
                         <div class="col-12">
                             <div class="footer_social_area text-center">
-                                <a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a>
                                 <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
                                 <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
                                 <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
